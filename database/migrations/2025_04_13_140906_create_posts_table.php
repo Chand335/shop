6095137a6a76465b0e2a1eebe483bd7365881894
Chandra Prakash <cprakashv07@gmail.com>
2025-04-13 20:10:41 +0530

    Add migrations and models for posts and post tags

---
+++ database/migrations/2025_04_13_140906_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('posts', function (Blueprint $table) {
+      $table->uuid('id')->primary();
+      $table->string('title');
+      $table->string('slug')->unique();
+      $table->longText('content');
+      $table->foreignUuid('created_by')->constrained('users')->onDelete('cascade');
+      $table->foreignUuid('category_id')
+        ->nullable()
+        ->constrained('post_categories')
+        ->nullOnDelete();
+      $table->date('published_at')->nullable()->index();
+      $table->string('image')->nullable();
+      $table->string('status')->default('draft')->index();
+      $table->softDeletes();
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('posts');
+  }
+};
