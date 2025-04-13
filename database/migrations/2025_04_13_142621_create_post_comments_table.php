648d74dcaf54da258c76fcc5d0d61e88668f2293
Chandra Prakash <cprakashv07@gmail.com>
2025-04-13 20:01:50 +0530

    Add migrations and models for posts, post categories, post tags, and post comments

---
+++ database/migrations/2025_04_13_142621_create_post_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_comments', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('post_id')->constrained('posts')->onDelete('cascade');
+            $table->foreignUuid('customer_id')->constrained('users')->onDelete('cascade');
+            $table->foreignUuid('parent_id')->constrained('post_comments')->onDelete('cascade');
+            $table->longText('content');
+            $table->boolean('is_visible')->default(true);
+            $table->boolean('is_approved')->default(true);
+            $table->foreignUuid('approved_by')->constrained('users')->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+            $table->index(['post_id', 'is_visible', 'is_approved']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_comments');
+    }
+};
---
+++ database/migrations/2025_04_13_151344_create_post_tag_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->foreignUuid('post_id')->constrained('posts')->onDelete('cascade');
+            $table->foreignUuid('tag_id')->constrained('post_tags')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_tag');
+    }
+};
